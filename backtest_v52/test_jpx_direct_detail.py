f4d46f653e16fd888ee805632695f0fb600ed5af
kazuma.miki <77340746+kazooooo-ma@users.noreply.github.com>
2026-08-22 23:03:34 +0900

    Assert direct JPX issuer detail works

--- backtest_v52/test_jpx_direct_detail.py
+++ backtest_v52/test_jpx_direct_detail.py
@@ -5,6 +5,8 @@ r=s.get('https://www2.jpx.co.jp/tseHpFront/JJK010010Action.do?Show=Show',timeout
 data={'BaseJh':'BaseJh','mgrCd':'72030','jjHisiFlg':'1','lstDspPg':'1','dspGs':'200','souKnsu':'1','sniMtGmnId':'JJK010010','dspJnKbn':'0','dspJnKmkNo':'0'}
 d=s.post('https://www2.jpx.co.jp/tseHpFront/JJK010030Action.do',data=data,timeout=30); d.encoding='utf-8'
 ss=BeautifulSoup(d.text,'html.parser')
+links=[a for a in ss.find_all('a',href=True) if 'ixbrl' in a['href']]
 print(d.status_code,len(d.text),ss.title.get_text(' ',strip=True) if ss.title else '')
-print(ss.get_text(' ',strip=True)[:1000])
-print('ixbrl',len([a for a in ss.find_all('a',href=True) if 'ixbrl' in a['href']]))
+print('ixbrl',len(links))
+if not links:
+    raise RuntimeError('Direct JPX issuer-detail access returned no iXBRL links')
